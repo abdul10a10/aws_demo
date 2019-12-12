e5aabbc3029a2b5a9b4476048beb3be633bc5249
10a10khan <abdulrahman.khan@codoxysolutions.com>
2019-12-12 11:36:43 +0530

    added secret in application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module EbRails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.secret_key_base = '1303e22a75d28e85f02d14945b1d86d659eb293b72b1371bf0e10763b7536865689df1a50412f9867a9b77c5a66e59ed86a81c8943b390320bbf5a98730aa7ac'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
